b58ebac56fa5a911be67b3eb0b5d9fa448bcd6c2
Peng.Jin <peng.jin@symbio.com>
2021-07-12 22:16:37 +0800

    basic configs

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ var Koa = require('koa')
 const router = require('koa-router')()
 
 var app = new Koa()
-router.get('/', function(ctx, next){
-  ctx.body = '你好，koa server send data';
-})
-router.get('/user', function(ctx, next){
-  ctx.body = 'user page coming';
-})
 
-app.use(router.routes())
+router
+  .get('/', function(ctx, next){
+  ctx.body = '你好，koa server send data';
+  })
+
+router.get('/user/:id', async (ctx, next) => {
+  ctx.body = `user id is ${ctx.params.id}`;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000)
