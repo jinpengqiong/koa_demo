7a5c0cbfb44a62694db0124912a6471eb48baff2
Peng.Jin <peng.jin@symbio.com>
2021-07-12 23:07:56 +0800

    added koa-static

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var Koa = require('koa')
 const router = require('koa-router')()
 
 var app = new Koa()
+app.use(async(ctx,next)=>{
+  console.log(new Date())
+  await next()
+})
 
 router
   .get('/', function(ctx, next){
@@ -10,6 +14,9 @@ router
 
 router.get('/user/:id', async (ctx, next) => {
   ctx.body = `user id is ${ctx.params.id}`;
+});
+router.get('/login', async (ctx, next) => {
+  ctx.body = `logon page`;
 });
 
 app.use(router.routes()).use(router.allowedMethods());
