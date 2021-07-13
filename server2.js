c81cad83e40acd77c8058f6943e26e1d9adbad5b
Peng.Jin <peng.jin@symbio.com>
2021-07-13 18:22:45 +0800

    added koa-session

---
+++ server2.js
@@ -0,0 +1,64 @@
+var Koa = require('koa');
+const router = require('koa-router')();
+const path = require('path');
+const session = require('koa-session');
+const render = require('koa-art-template');
+const bodyParser = require('koa-bodyparser');
+// const serve = require('koa-static');
+
+var app = new Koa();
+app.keys = ['some secret hurr'];
+
+// * koa-art-template
+render(app, {
+  root: path.join(__dirname, 'views'),
+  extname: '.html',
+  debug: process.env.NODE_ENV !== 'production',
+});
+
+app.use(bodyParser());
+
+// * koa-session
+app.use(session(app));
+
+app.use(async (ctx, next) => {
+  console.log(new Date());
+  ctx.state.userInfo = {
+    userName: 'Kim',
+    age: 30,
+  };
+  // * koa-session actions
+  console.log(`ctx.session`, ctx.session)
+  let n = ctx.session.views || 0;
+  ctx.session.views = ++n;
+  ctx.body = n + ' views';
+  await next();
+});
+
+router.get('/', async (ctx, next) => {
+  // const content = '<h2>this is a html variable</h2>';
+  // const userInfo = Buffer.alloc(6, '金鹏').toString('base64');
+  // ctx.cookies.set('userInfo', userInfo, {
+  //   maxAge: 60 * 1000 * 60 * 24,
+  //   httpOnly: true,
+  // });
+  // await ctx.render('index.html', {
+  //   list: ['ejs Modules', 'Micheal Jordan', 'Micheal Jackson'],
+  //   content,
+  // });
+});
+
+router.get('/user/:id', async (ctx, next) => {
+  // * 设置中文cookies
+  ctx.body = `user id is ${ctx.params.id}, userinfo : ${Buffer.alloc(6, ctx.cookies.get('userInfo'), 'base64').toString()}`;
+});
+router.get('/login', async (ctx, next) => {
+  ctx.body = `logon page`;
+});
+router.post('/form_submit', async (ctx, next) => {
+  ctx.body = ctx.request.body;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000);
