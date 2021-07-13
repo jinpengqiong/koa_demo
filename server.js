62a567dcfe20c6af3c26f2863254cde28ba95b56
Peng.Jin <peng.jin@symbio.com>
2021-07-13 11:18:30 +0800

    ok

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 var Koa = require('koa')
 const router = require('koa-router')()
 const views = require('koa-views')
+const bodyParser = require('koa-bodyparser');
 
 var app = new Koa()
 // * koa-views
 const render = views('views', { extension: 'ejs' });
 app.use(render);
+app.use(bodyParser())
 
 app.use(async(ctx,next)=>{
   console.log(new Date())
   await next()
 })
 
+app.use(async(ctx, next) => {
+  ctx.state.userInfo = {
+    userName:'Kim',
+    age: 30
+  }
+  await next();
+})
+
 router
   .get('/', async(ctx, next) => {
+    const content = '<h2>this is a html variable</h2>'
     await ctx.render('index.ejs', {
-      list: ['ejs Modules','Micheal Jordan','Micheal Jackson']
+      list: ['ejs Modules', 'Micheal Jordan', 'Micheal Jackson'],
+      content
     });
   })
 
@@ -24,6 +36,9 @@ router.get('/user/:id', async (ctx, next) => {
 });
 router.get('/login', async (ctx, next) => {
   ctx.body = `logon page`;
+});
+router.post('/form_submit', async (ctx, next) => {
+  ctx.body = ctx.request.body
 });
 
 app.use(router.routes()).use(router.allowedMethods());
